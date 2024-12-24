9109e5beb3a1eba1078e2334599b039a9a8b7ccf
Andrea Alberti <a.alberti82@gmail.com>
2024-12-24 19:53:54 +0100

    Debugging cmds

--- tmux-ssh-syncing.plugin.zsh
+++ tmux-ssh-syncing.plugin.zsh
@@ -2,8 +2,10 @@
 
 # Copyright (c) 2024, Andrea Alberti
 
+# Safe method to retrieve the plugin path
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
+# ${0:h} -> plugin directory
 fpath=("${0:h}/src" $fpath)
 autoload -Uz ssh
